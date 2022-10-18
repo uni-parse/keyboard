755fb088633860ddcf63396801e7cf37e328ac84
theuniparse <theuniparse@gmail.com>
2022-10-18 11:31:47 +0100

    optimize layerSwitcher event listener

--- src/layerSwitcher.js
+++ src/layerSwitcher.js
@@ -2,15 +2,15 @@ export default layerSwitcher
 import keys from "./keys";
 
 function layerSwitcher(fieldset) {
-  fieldset.addEventListener('change', () => {
-    let layer = document.querySelector('input[name=layer]:checked').value
-
+  fieldset.addEventListener('change', event => {
     for (const row in keys) {
       const buttons = document.querySelectorAll(`#${row}>button`)
+
+      //update layer buttons keys
+      let layer = event.target.value //input[type=radio].value
       for (let i = 0; i < buttons.length; i++) {
         buttons[i].textContent = keys[row][i][layer]
       }
-
     }
   })
 }
