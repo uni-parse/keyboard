8d124adc74851051f4c59ad73a9c65b6514e8491
theuniparse <theuniparse@gmail.com>
2022-12-23 12:16:01 +0100

    reStyle <pre> script

--- src/scriptAhk.js
+++ src/scriptAhk.js
@@ -4,6 +4,7 @@ const pre = document.createElement('pre')
 pre.innerText = autohotkeyStr
 
 const copyBtn = document.createElement('button')
+copyBtn.id = 'copyBtn'
 copyBtn.textContent = 'copy'
 copyBtn.addEventListener('click', () =>
   navigator.clipboard.writeText(autohotkeyStr)
@@ -11,7 +12,7 @@ copyBtn.addEventListener('click', () =>
 )
 
 const script = document.createElement('div')
-script.id = 'script'
+script.id = 'autoHotKeyCtx'
 script.appendChild(copyBtn)
 script.appendChild(pre)
 
