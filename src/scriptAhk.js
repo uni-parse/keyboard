01b8d573a67691947b29c20b730741a9fd8e1729
theuniparse <theuniparse@gmail.com>
2022-12-23 02:51:07 +0100

    optimize layout.js

---
+++ src/scriptAhk.js
@@ -0,0 +1,79 @@
+
+import keys from './keys'
+import autohotkeyStr from './autohotkey'
+import layout from './layout'
+import showMedias from './mediaIcons'
+
+
+const pre = document.createElement('pre'),
+  btn = document.createElement('button'),
+  script = document.createElement('div'),
+  download = document.createElement('div'),
+  msklc = document.createElement('a'),
+  ahk = document.createElement('a')
+
+script.id = 'script'
+btn.textContent = 'copy'
+msklc.textContent = 'MSKLC.zip'
+msklc.href = './../src/assets/uniparse/msklc.zip'
+msklc.download = 'msklc.zip'
+ahk.textContent = 'AUTOHOTKEY.zip'
+ahk.href = './../src/assets/uniparse/autohotkey.zip'
+ahk.download = 'autohotkey.zip'
+download.id = 'download'
+download.appendChild(msklc)
+download.appendChild(ahk)
+script.appendChild(btn)
+script.appendChild(pre)
+
+main.appendChild(download)
+main.appendChild(script)
+
+
+
+pre.innerText = autohotkeyStr
+btn.addEventListener('click', () => {
+  navigator.clipboard.writeText(autohotkeyStr)
+    .then(() => btn.textContent = 'copied')
+})
+layout(keys, keyboard)
+
+let toggle_extend = false, toggle_symbol = false
+const span_key = document.querySelectorAll('.key'), span_extend = document.querySelectorAll('.extend'),
+  span_symbol = document.querySelectorAll('.symbol'),
+  span_symbol2 = document.querySelectorAll('.symbol2'),
+  extend = document.querySelector('#keyboard>span:nth-child(59)'),
+  symbol = document.querySelector('#keyboard>span:nth-child(57)')
+function display(keys = 1, extend = 0, symbol = 0, symbol2 = 0) {
+  span_key.forEach(key => key.style.opacity = keys)
+  span_extend.forEach(key => key.style.opacity = extend);
+  span_symbol.forEach(key => key.style.opacity = symbol)
+  span_symbol2.forEach(key => key.style.opacity = symbol2)
+}
+extend.addEventListener('click', () => {
+  toggle_extend = !toggle_extend
+  if (toggle_extend && !toggle_symbol) {
+    display(0, 1)
+  } else if (toggle_extend && toggle_symbol) {
+    display(0, 0, 0, 1)
+  } else if (!toggle_extend && toggle_symbol) {
+    display()
+    toggle_symbol = 0
+  } else {
+    display()
+  }
+})
+
+symbol.addEventListener('click', () => {
+  toggle_symbol = !toggle_symbol
+  if (!toggle_extend && toggle_symbol) {
+    display(0, 0, 1)
+  } else if (toggle_extend && toggle_symbol) {
+    display(0, 0, 0, 1)
+  } else if (toggle_extend && !toggle_symbol) {
+    display()
+    toggle_extend = 0
+  } else {
+    display()
+  }
+})
