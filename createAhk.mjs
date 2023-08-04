5ea2c125c3df45927c9e22226a354fed205a0d06
theUniParse <theUniParse@gmail.com>
2023-08-04 16:21:25 +0100

    pre init next-router

--- createAhk.mjs
+++
@@ -1,8 +0,0 @@
-import { writeFile } from 'node:fs'
-import { autohotkeyStr } from './src/ahk.mjs'
-
-writeFile(
-  './src/assets/UniParse.ahk',   //target file
-  autohotkeyStr,                 //data
-  err => { if (err) throw err }  //error handler
-)
--- src/assets/test.ahk
+++
@@ -1,4 +0,0 @@
-﻿#Requires AutoHotkey v2.0
-#Warn
-key := "msg"
-2::MsgBox(key . 2)
